a60b27308de2b644ea90f982a7388fb64dacfe0f
EvilHui <Ukrumoski@gmail.com>
2024-11-28 23:21:49 +0500

    task 4

---
+++ Tyuiu.KimmelDS.Sprint3.Task4.V8.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KimmelDS.Sprint3.Task4.V8.Lib
+{
+    public class DataService : ISprint3Task4V8
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            double result = 1; 
+
+            for (double x = startValue; x <= stopValue; x += 0.1) 
+            {
+                try
+                {
+                    if (x == 0)
+                    {
+                        break; 
+                    }
+                    double y = (x / Math.Sin(x)) + 0.5;
+                    result *= y;
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine($"Ошибка: Деление на ноль при x = {x}");
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.KimmelDS.Sprint3.Task4.V8.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.KimmelDS.Sprint3.Task4.V8.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}

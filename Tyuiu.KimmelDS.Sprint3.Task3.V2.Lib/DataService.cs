f227dc7b470f4da835f92f75e625b778757ce52f
EvilHui <Ukrumoski@gmail.com>
2024-11-28 22:16:13 +0500

    task 3

---
+++ Tyuiu.KimmelDS.Sprint3.Task3.V2.Lib/DataService.cs
@@ -0,0 +1,9 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KimmelDS.Sprint3.Task3.V2.Lib
+{
+    public class DataService : ISprint3Task3V2
+    {
+        public int GetMaxCharCount(string value, char item) => value.Count(c => c == item);
+    }
+}

20efd2ed6328c58942207122dfd312122a64b840
EvilHui <Ukrumoski@gmail.com>
2024-11-28 19:35:16 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.KimmelDS.Sprint3.Task0.V26.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KimmelDS.Sprint3.Task0.V26.Lib
+{
+    public class DataService : ISprint3Task0V26
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double product = 1.0;
+
+            for (int k = startValue; k <= stopValue; k++)
+                product *= Math.Pow(2, k) / (k + 1);
+
+            return product * Math.Cos(1.8);
+        }
+    }
+}
---
+++ Tyuiu.KimmelDS.Sprint3.Task0.V26/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.KimmelDS.Sprint3.Task0.V26.Lib;
+namespace Tyuiu.KimmelDS.Sprint3.Task0.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Киммель Дмитрий Сергеевич | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Операции сравнения                                                      *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Киммель Дмитрий Сергеевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Написать программу из операций сравнений (==, !=, <, >, <=, >=, последова*");
+            Console.WriteLine("*ательность операций не должна нарушаться) и арифметических выражений, кот*");
+            Console.WriteLine("*торая вернет логическую последовательность(массив): (True, True, True, Tr*");
+            Console.WriteLine("*rue, True, False), при x = 1054, y = 375                                 *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.GetMultiplySeries(1, 2);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}

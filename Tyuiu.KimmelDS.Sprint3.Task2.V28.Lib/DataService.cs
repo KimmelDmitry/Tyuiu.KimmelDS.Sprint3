aefea1804cd67ee20672fc95970845307fb356db
EvilHui <Ukrumoski@gmail.com>
2024-11-28 22:01:37 +0500

    task 2 pure hate

---
+++ Tyuiu.KimmelDS.Sprint3.Task2.V28.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KimmelDS.Sprint3.Task2.V28.Lib
+{
+    public class DataService : ISprint3Task2V28
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double result = 0;
+            var k = startValue;
+            do
+            {
+                result = (Math.Pow(2, k) /  (k + 1)) * Math.Cos(1.8);
+            } while (k <= stopValue);
+
+            return result;
+        }
+    }
+}

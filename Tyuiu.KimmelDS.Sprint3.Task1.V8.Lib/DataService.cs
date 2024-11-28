f958c74bc2828d7ebbfb5d6885218be0e68f55fe
EvilHui <Ukrumoski@gmail.com>
2024-11-28 21:20:22 +0500

    task1

---
+++ Tyuiu.KimmelDS.Sprint3.Task1.V8.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KimmelDS.Sprint3.Task1.V8.Lib
+{
+    public class DataService : ISprint3Task1V8
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double result = 0;
+            for (int k = startValue; k <= stopValue; k++)
+            {
+                result += Math.Pow(1 / (Math.Cos(k) + Math.Pow(value, k)), k);
+            }
+            return value;
+        }
+    }
+}
---
+++ Tyuiu.KimmelDS.Sprint3.Task1.V8.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.KimmelDS.Sprint3.Task1.V8.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}

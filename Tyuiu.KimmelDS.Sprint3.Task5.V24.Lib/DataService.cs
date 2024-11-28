76472ec8f3527721e7027df5307ecab1c6b965b8
EvilHui <Ukrumoski@gmail.com>
2024-11-28 23:56:15 +0500

    task 5

---
+++ Tyuiu.KimmelDS.Sprint3.Task5.V24.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KimmelDS.Sprint3.Task5.V24.Lib
+{
+    public class DataService : ISprint3Task5V24
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double y = 0; 
+            for (int i = startValue1; i <= stopValue1; i++)
+                for (int k = startValue2; k <= stopValue2; k++)
+                    y += (Math.Pow(x, k) + 2) * Math.Sin(k);
+            return y; 
+        }
+    }
+}
---
+++ Tyuiu.KimmelDS.Sprint3.Task5.V24/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.KimmelDS.Sprint3.Task5.V24.Lib;
+namespace Tyuiu.KimmelDS.Sprint3.Task5.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Киммель Дмитрий Сергеевич | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Операции сравнения                                                      *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнил: Киммель Дмитрий Сергеевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Написать программу из операций сравнений (==, !=, <, >, <=, >=, последова*");
+            Console.WriteLine("*ательность операций не должна нарушаться) и арифметических выражений, кот*");
+            Console.WriteLine("*торая вернет логическую последовательность(массив): (True, True, True, Tr*");
+            Console.WriteLine("*rue, True, False), при x = 1054, y = 375                                 *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.GetSumSumSeries(1, 1, 3, 1, 3);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
